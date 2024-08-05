2c2db7d12d000be73e1820387a34313511344d14
Sero <69639595+Seroxdesign@users.noreply.github.com>
2024-08-05 04:56:09 -0400

    impl in service

--- service/src/blocking.rs
+++ service/src/blocking.rs
@@ -1,8 +1,14 @@
+use ark_bn254;
+use ark_ec::TEModelParameters as TE;
 use ivcnotes::circuit::IVC;
 use ivcnotes::service::msg;
 use ivcnotes::Error;
 use ivcnotes::{circuit::concrete::Concrete, service::Service};
+use rand::rngs::OsRng;
 use reqwest::{Method, Url};
+use sha2;
+use std::cell::RefCell;
+use std::collections::HashMap;
 
 pub enum HttpScheme {
     Http,
@@ -14,15 +20,26 @@ enum Path {
     GetContact,
     SendNote,
     GetNotes,
+    CreateUser,
+    GetUser,
+    CreateAndTransferNoteHistory,
+    GetNoteHistoryForUser,
+    StoreNullifier,
+    VerifyNullifier,
 }
 
 pub struct BlockingHttpClient {
     scheme: HttpScheme,
     host: String,
     port: Option<u16>,
+    shared: RefCell<SharedState>,
 }
 
 type Field = <Concrete as IVC>::Field;
+
+struct SharedState {
+    contacts: HashMap<Address<ark_bn254::Fr>, Contact<Concrete>>,
+}
 
 fn send<Req: serde::Serialize, Res: for<'de> serde::Deserialize<'de>>(
     method: Method,
@@ -48,6 +65,9 @@ impl BlockingHttpClient {
             scheme,
             host: host.to_string(),
             port,
+            shared: RefCell::new(SharedState {
+                contacts: HashMap::new(),
+            }),
         }
     }
 
@@ -68,36 +88,231 @@ impl BlockingHttpClient {
             Path::GetContact => "get_contact",
             Path::GetNotes => "get_notes",
             Path::SendNote => "send_note",
+            Path::CreateUser => "create_user",
+            Path::GetUser => "get_user",
+            Path::CreateAndTransferNoteHistory => "create_and_transfer_note_history",
+            Path::GetNoteHistoryForUser => "get_note_history_for_user",
         };
         url.set_path(path);
         url
+    }
+
+    fn get_user_from_db(&self, identifier: UserIdentifier) -> Result<Contact<Concrete>, String> {
+        let url = self.path(Path::GetUser);
+        let wrapper = IdentifierWrapper { identifier };
+        let res: User =
+            send(Method::GET, url, &wrapper).map_err(|e| format!("Failed to get user: {}", e))?;
+
+        let address: Address<ark_bn254::Fr> = match res.address {
+            Some(ref address_str) => {
+                let smtg: SmtgWithAddress<ark_bn254::Fr> =
+                    serde_json::from_str(address_str).expect("no address");
+                smtg.address
+            }
+            None => return Err("User address is None".into()),
+        };
+
+        let pubkey: PublicKey<TE> = match res.pubkey {
+            Some(ref pubkey_str) => {
+                let smtg: SmtgWithPubkey<TE> = serde_json::from_str(pubkey_str).expect("no pubkey");
+                smtg.pubkey
+            }
+            None => return Err("User pubkey is None".into()),
+        };
+
+        Ok(Contact {
+            public_key: pubkey,
+            address,
+            username: res.username.ok_or("Username is None")?,
+        })
     }
 }
 
 impl Service<Concrete> for BlockingHttpClient {
     fn register(&self, msg: &msg::request::Register<Concrete>) -> Result<(), Error> {
-        let path = self.path(Path::Register);
-        send(Method::POST, path, msg)
+        let sk: SigningKey<TE> = SigningKey::generate::<sha2::Sha512>(&mut OsRng).unwrap();
+        let pubkey = sk.public_key().clone();
+        let smtg_pubkey = SmtgWithPubkey { pubkey };
+        let smtg_address = SmtgWithAddress {
+            address: msg.address,
+        };
+        let pubkey_json = serde_json::to_string(&smtg_pubkey).unwrap();
+        let address_json = serde_json::to_string(&smtg_address).unwrap();
+
+        let create_user_schema = CreateUserSchema {
+            username: msg.username.clone(),
+            address: address_json,
+            pubkey: pubkey_json,
+            nonce: String::new(),
+            messages: Vec::new(),
+            notes: Vec::new(),
+            has_double_spent: false,
+        };
+
+        let url = self.path(Path::CreateUser);
+        send(Method::POST, url, &create_user_schema)
     }
 
     fn get_contact(
         &self,
         msg: &msg::request::GetContact<Field>,
     ) -> Result<msg::response::Contact<Concrete>, Error> {
-        let path = self.path(Path::GetContact);
-        send(Method::GET, path, msg)
+        let mut shared = self.shared.borrow_mut();
+
+        let contact = match msg {
+            msg::request::GetContact::Username(username) => shared
+                .contacts
+                .values()
+                .find(|contact| contact.username == *username)
+                .cloned()
+                .or_else(|| {
+                    self.get_user_from_db(UserIdentifier::Username(username.clone()))
+                        .ok()
+                }),
+            msg::request::GetContact::Address(address) => {
+                shared.contacts.get(address).cloned().or_else(|| {
+                    let smtg_address = SmtgWithAddress { address: *address };
+                    let address_json = serde_json::to_string(&smtg_address).unwrap();
+                    self.get_user_from_db(UserIdentifier::Address(address_json))
+                        .ok()
+                })
+            }
+        };
+
+        match contact {
+            Some(contact) => {
+                shared
+                    .contacts
+                    .entry(contact.address)
+                    .or_insert_with(|| contact.clone());
+                Ok(contact)
+            }
+            None => Err(Error::With("contact not found")),
+        }
     }
 
     fn send_note(&self, msg: &msg::request::Note<Concrete>) -> Result<(), Error> {
-        let path = self.path(Path::SendNote);
-        send(Method::POST, path, msg)
+        let smtg_address = SmtgWithAddress {
+            address: msg.receiver,
+        };
+        let address_json = serde_json::to_string(&smtg_address).unwrap();
+
+        let send_and_transfer_json = NoteHistoryRequest {
+            owner_username: None,
+            recipient_username: msg.username.clone(),
+            note_history: SaveNoteHistoryRequestSchema {
+                data: msg.note_history.encrypted.data.clone(),
+                address: address_json,
+            },
+            message: "Transfer".to_string(),
+        };
+
+        let url = self.path(Path::CreateAndTransferNoteHistory);
+        send(Method::POST, url, &send_and_transfer_json)
     }
 
     fn get_notes(
         &self,
         msg: &msg::request::GetNotes<Field>,
     ) -> Result<msg::response::Notes<Concrete>, Error> {
-        let path = self.path(Path::GetNotes);
-        send(Method::POST, path, msg)
+        let username_request = UsernameRequest {
+            username: msg.username.clone(),
+        };
+
+        let url = self.path(Path::GetNoteHistoryForUser);
+        let note_history: Vec<NoteHistorySaved> = send(Method::GET, url, &username_request)?;
+
+        let encrypted_note_history: Vec<EncryptedNoteHistory<Concrete>> = note_history
+            .into_iter()
+            .map(|nh| self.convert_note_history_to_encrypted_note_history(nh, msg.username.clone()))
+            .collect();
+
+        Ok(msg::response::Notes {
+            notes: encrypted_note_history,
+        })
+    }
+
+    pub fn store_nullifier(
+        &self,
+        nullifier: String,
+        state: String,
+        owner: String,
+    ) -> Result<crate::service_schema::NullifierResponseData, String> {
+        let nullifier_schema = crate::service_schema::NoteNullifierSchema {
+            nullifier,
+            state,
+            owner,
+            step: 1,
+            note: "1".to_owned(),
+        };
+
+        let url = self.path(Path::StoreNullifier);
+        let res: serde_json::Value = send(Method::POST, url, &nullifier_schema)
+            .map_err(|e| format!("Failed to store nullifier: {}", e))?;
+
+        let status = match res["status"].as_str() {
+            Some("success") => "success",
+            Some("not_found") => "not_found",
+            _ => "error",
+        };
+
+        let nullifier = serde_json::from_value(res["nullifier"].clone())
+            .map_err(|e| format!("Failed to parse nullifier: {}", e))?;
+
+        Ok(crate::service_schema::NullifierResponseData { status, nullifier })
+    }
+
+    pub fn get_nullifier(
+        &self,
+        nullifier: String,
+        expected_state: String,
+    ) -> Result<crate::service_schema::NullifierResponse, String> {
+        let nullifier_request = crate::service_schema::NullifierRequest {
+            nullifier,
+            state: expected_state,
+        };
+
+        let url = self.path(Path::VerifyNullifier);
+        let result: Result<serde_json::Value, Error> = send(Method::GET, url, &nullifier_request);
+
+        match result {
+            Ok(json) => {
+                let status = match json["status"].as_str() {
+                    Some("success") => "success",
+                    Some("not_found") => "not_found",
+                    _ => "error",
+                };
+
+                let nullifier = serde_json::from_value(json["nullifier"].clone())
+                    .map_err(|e| format!("Failed to parse nullifier: {}", e))?;
+
+                let response_data =
+                    crate::service_schema::NullifierResponseData { status, nullifier };
+                Ok(crate::service_schema::NullifierResponse::Ok(
+                    response_data.nullifier,
+                ))
+            }
+            Err(Error::Service(e)) if e.contains("404 Not Found") => {
+                Ok(crate::service_schema::NullifierResponse::NotFound)
+            }
+            Err(_) => Ok(crate::service_schema::NullifierResponse::Error),
+        }
+    }
+}
+
+impl BlockingHttpClient {
+    fn convert_note_history_to_encrypted_note_history(
+        &self,
+        nh: NoteHistorySaved,
+        username: String,
+    ) -> EncryptedNoteHistory<Concrete> {
+        let contact = self
+            .get_user_from_db(UserIdentifier::Username(username))
+            .expect("couldn't get contact");
+
+        EncryptedNoteHistory {
+            sender: contact,
+            encrypted: EncryptedData { data: nh.data },
+        }
     }
 }
---
+++ service/src/schema.rs
@@ -0,0 +1,195 @@
+use mongodb::bson::{doc, Bson};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct User {
+    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+    pub id: Option<String>,
+    pub has_double_spent: Option<bool>,
+    pub nonce: Option<String>,
+    pub username: Option<String>,
+    pub pubkey: Option<String>,
+    pub messages: Option<Vec<String>>,
+    pub notes: Option<Vec<bson::oid::ObjectId>>,
+    pub address: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct CreateUserSchema {
+    pub username: String,
+    pub pubkey: String,
+    pub nonce: String,
+    pub address: String,
+    pub messages: Vec<String>,
+    pub notes: Vec<String>,
+    pub has_double_spent: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct UserRequest {
+    pub identifier: UserIdentifier,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum UserIdentifier {
+    Username(String),
+    Address(String),
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct UsernameRequest {
+    pub username: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct NoteSchema {
+    pub(crate) asset_hash: String,
+    pub(crate) owner: String,
+    pub(crate) value: u64,
+    pub(crate) step: u32,
+    pub(crate) parent_note: String,
+    pub(crate) out_index: String,
+    pub(crate) blind: String,
+    pub(crate) _id: Option<Bson>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct SaveNoteRequestSchema {
+    pub(crate) asset_hash: String,
+    pub(crate) owner: String,
+    pub(crate) value: u64,
+    pub(crate) step: u32,
+    pub(crate) parent_note: String,
+    pub(crate) out_index: String,
+    pub(crate) blind: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct SaveNoteHistoryRequestSchema {
+    pub data: Vec<u8>,
+    pub address: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct NoteHistorySaved {
+    pub data: Vec<u8>,
+    pub address: String,
+    pub _id: Option<Bson>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct NoteHistorySchema {
+    note: NoteSchema,
+    history: Vec<NoteSchema>,
+    spent: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct MessageSchema {
+    pub recipient: String,
+    pub sender: String,
+    pub message: String,
+    pub timestamp: i64,
+    pub attachment_id: Option<Bson>,
+    pub read: bool,
+    pub _id: Option<Bson>,
+}
+#[derive(Serialize, Deserialize, Debug)]
+pub struct MessageRequestSchema {
+    pub recipient: String,
+    pub sender: String,
+    pub message: String,
+    pub attachment_id: Option<Bson>,
+}
+
+// We must add a future state vector
+#[derive(Debug, Deserialize, Serialize)]
+pub struct NoteNullifierSchema {
+    pub nullifier: String,
+    pub note: String, // Note structure serialized as JSON
+    pub step: i32,
+    pub owner: String, // Address serialized as JSON
+    pub state: String,
+}
+#[derive(Debug, Deserialize, Serialize)]
+pub struct NullifierRequest {
+    pub nullifier: String,
+    pub state: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct NoteRequest {
+    pub owner_pub_key: String,
+    pub step: Option<u32>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct NoteHistoryRequest {
+    pub owner_username: Option<String>,
+    pub recipient_username: String,
+    pub note_history: SaveNoteHistoryRequestSchema,
+    pub message: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ChallengeSchema {
+    pub challenge_id: String,
+    pub user_id: String,
+    pub created_at: i64,
+    pub expires_at: i64,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AuthData {
+    pub username: String,
+    pub signature_hex: String,
+    pub challenge_id: String,
+}
+
+#[derive(Debug, Serialize, Clone)]
+pub struct UserSingleResponse {
+    pub status: &'static str,
+    pub user: User,
+}
+
+#[derive(Debug, Serialize)]
+pub struct MessageSingleResponse {
+    pub status: &'static str,
+    pub message: MessageSchema,
+}
+#[derive(Debug, Serialize, Deserialize)]
+pub struct NullifierResponseData {
+    pub status: &'static str,
+    pub nullifier: NoteNullifierSchema,
+}
+#[derive(Debug, Serialize)]
+pub enum NullifierResponse {
+    Ok(NoteNullifierSchema),
+    NotFound,
+    Error,
+}
+
+#[derive(Debug, Serialize)]
+pub struct NoteResponse {
+    pub status: &'static str,
+    pub note: NoteSchema,
+}
+
+#[derive(Debug, Serialize)]
+pub struct NoteHistoryResponse {
+    pub status: &'static str,
+    pub note_history: NoteHistorySaved,
+}
+
+#[derive(Serialize)]
+pub struct IdentifierWrapper {
+    pub identifier: UserIdentifier,
+}
+
+pub struct Contact<E: IVC> {
+    #[serde(with = "crate::ark_serde")]
+    pub(crate) address: Address<E::Field>,
+    pub(crate) username: String,
+    #[serde(with = "crate::ark_serde")]
+    pub(crate) public_key: PublicKey<E::TE>,
+}
